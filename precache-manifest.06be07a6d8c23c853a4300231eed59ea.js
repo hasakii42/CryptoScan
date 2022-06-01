1cdd68250a09c00b2eb081818f10e085e50ead35
hasakii42 <63420887+hasakii42@users.noreply.github.com>
2022-06-01 16:43:43 +0530

    Updates

---
+++ precache-manifest.06be07a6d8c23c853a4300231eed59ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "17e1b87eb5fe3b5c0012",
+    "url": "/CryptoScan/static/css/main.63057b40.chunk.css"
+  },
+  {
+    "revision": "17e1b87eb5fe3b5c0012",
+    "url": "/CryptoScan/static/js/main.17e1b87e.chunk.js"
+  },
+  {
+    "revision": "5a3ad488897027c6435a",
+    "url": "/CryptoScan/static/css/1.4429990d.chunk.css"
+  },
+  {
+    "revision": "5a3ad488897027c6435a",
+    "url": "/CryptoScan/static/js/1.5a3ad488.chunk.js"
+  },
+  {
+    "revision": "85d613812d5467759fec",
+    "url": "/CryptoScan/static/js/runtime~main.85d61381.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/CryptoScan/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "0e213db57026d369d3cc613fb3bafcbf",
+    "url": "/CryptoScan/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CryptoScan/precache-manifest.06be07a6d8c23c853a4300231eed59ea.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CryptoScan/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
